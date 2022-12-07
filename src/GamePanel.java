2b1f9c64a3ed24736e6153b52c26274d1d345b3e
Eighty_Bity <byavik2@gmail.com>
2022-12-07 09:14:25 -0500

    started inputs

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,10 +1,24 @@
 import javax.swing.JPanel;
 import java.awt.Graphics;
+import java.awt.event.KeyListener;
+import java.awt.event.KeyEvent;
 
 
 public class GamePanel extends JPanel{
     public GamePanel() {
+        addKeyListener(new KeyListener() {
+                public void keyTyped(KeyEvent e){
+                    
+                }
+                public void keyPressed(KeyEvent e){
 
+                }
+                public void keyReleased(KeyEvent e) {
+                    // TODO Auto-generated method stub
+                    
+                }
+            }
+        );
     }
     public void paintComponent(Graphics g){
 
